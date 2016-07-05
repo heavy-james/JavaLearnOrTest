1d8b68ab21848c6cf98528476601b966e3375992
zhanhaifei <zhanhaifei@126.com>
2016-07-05 19:37:33 +0800

    1. complete logic of dtd attribute analyse

--- src/learn/zhf/test/bean/RegularExpressionTest.java
+++ src/learn/zhf/test/bean/RegularExpressionTest.java
@@ -1,5 +1,7 @@
 package learn.zhf.test.bean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,42 +12,102 @@ public class RegularExpressionTest extends TestCase{
 	
 	public static final String TAG = "RegularExpressionTest";
 	
-	public static final String REGULAR_NAME = "(\\w+(-\\w+)*)";
+	public static final String REGULAR_NAME = "\\w+(-\\w+)*";
 	
-	private static final String REGULAR_ENUM_ATTR_TYPE = "(\\(\\s*" + REGULAR_NAME  + "\\s*(\\|\\s*" + REGULAR_NAME + "\\s*)*\\s*\\)"
-			+"|\\s*CDATA\\s*|\\s*ID\\s*|\\s*IDREF\\s*|\\s*IDREFS\\s*|\\s*NMTOKEN\\s*|\\s*NMTOKENS\\s*|\\s*NOTATION\\s*|\\s*xml:\\w+\\s*)";
+	public static final String REGULAR_XML_NAME = "\\s*xml:\\w+\\s*";
+
+	public static final String REGULAR_ENUM_NAMES = "\\(\\s*" + REGULAR_NAME
+			+ "\\s*(\\|\\s*" + REGULAR_NAME + "\\s*)*\\s*\\)";
 	
-	private static final String REGULAR_ENUM_ATTR_VALUE = "(\\(\\s*" + REGULAR_NAME  + "\\s*(\\|\\s*" + REGULAR_NAME + "\\s*)*\\s*\\)|\\s*#REQUIRED\\s*|\\s*#IMPLIED\\s*|\\s*#FIXED\\s*\\w*)";
+	private static final String REGULAR_ENTITY_REFERENCE = "\\s*%\\w+(-\\w+)*;\\s*";
 	
-	private static final String REGULAR_ATTRI_NAME_TYPE_VALUE = "(\\s*" + REGULAR_NAME + "\\s+" + REGULAR_ENUM_ATTR_TYPE + "\\s+" + REGULAR_ENUM_ATTR_VALUE + "\\s*)";
-	
-	private static final String REGULAR_ENTITY_REFERENCE =  "%\\w+(-\\w+)*;";
-	
-	private static final String REGULAR_ATTR_CONTENT = "(\\s*" + REGULAR_ATTRI_NAME_TYPE_VALUE + "|" + REGULAR_ENTITY_REFERENCE + "\\s*)";
+	public static final String REGULAR_ATTRI_NAME = "("+ REGULAR_NAME + "|"+ REGULAR_XML_NAME +")";
+
+	private static final String REGULAR_ATTR_ENUM_VALUE = "("
+			+ REGULAR_ENUM_NAMES + "|" + REGULAR_ENTITY_REFERENCE 
+			+ "|\\s*CDATA\\s*|\\s*ID\\s*|\\s*IDREF\\s*|\\s*IDREFS\\s*|\\s*NMTOKEN\\s*|\\s*NMTOKENS\\s*|\\s*NOTATION\\s*)";
+
+	private static final String REGULAR_ATTR_ENUM_EXISTENCE = "(\\s*#REQUIRED\\s*|\\s*#IMPLIED\\s*|\\s*#FIXED\\s*\\w*)";
+
+	private static final String REGULAR_ATTRI_NAME_VALUE_EXISTENCE = "\\s*"
+			+ REGULAR_ATTRI_NAME + "\\s+" + REGULAR_ATTR_ENUM_VALUE + "\\s+"
+			+ REGULAR_ATTR_ENUM_EXISTENCE + "\\s*";
+
+
+	private static final String REGULAR_ATTR_CONTENT = "(\\s*"
+			+ REGULAR_ATTRI_NAME_VALUE_EXISTENCE + "|" + REGULAR_ENTITY_REFERENCE
+			+ "\\s*)";
 	
 	@Override
 	public void run() {
-		String testString = "<!ATTLIST staff-details" +
+		
+		
+		//boolean res = Pattern.matches(REGULAR_ENTITY_REFERENCE, " %print-object;");
+		
+		
+		String content = "<!ATTLIST staff-details" +
 				" number         CDATA                #IMPLIED " +
 				" show-frets     (numbers | letters)  #IMPLIED " +
+				"type %start-stop; #REQUIRED" +
+				" id ID #REQUIRED" +
 				" %print-object; " +
 				" %print-spacing; " +
 				" xml:lang NMTOKEN #IMPLIED>";
 			
-		Pattern pattern = Pattern.compile(REGULAR_ATTR_CONTENT);
-		
-		Matcher matcher =	pattern.matcher(testString);
-		boolean find = matcher.find();
-		while(find){
-			Log.d(TAG, "find attri of element-->" + matcher.group());
-			int start = matcher.start();
-			//Log.d(TAG, "find attri of element startPos-->" + start);
-			int end = matcher.end();
-			//Log.d(TAG, "find attri of element endPos-->" + end);
-			//testString = testString.substring(start + matcher.group().length());
-			find = matcher.find();
+
+		if (content != null && content.startsWith("<!ATTLIST")
+				&& content.endsWith(">")) {
+			content = content.substring("<!ATTLIST".length(), content.length()
+					- ">".length());
+			Pattern pattern = Pattern.compile(REGULAR_NAME);
+			Matcher matcher = pattern.matcher(content);
+			String elementName = null;
+			if (matcher.find()) {
+				elementName = matcher.group();
+				content = content.substring(matcher.start()
+						+ elementName.length());
+			} else {
+				
+			}
+
+			List<String> attriStrs = null;
+
+			if (elementName != null) {
+				pattern = Pattern.compile(REGULAR_ATTR_CONTENT);
+				matcher = pattern.matcher(content);
+				if (matcher.find()) {
+					attriStrs = new ArrayList<String>();
+					do {
+						String attrStr = matcher.group();
+						Log.d(TAG, "catch attri-->" + attrStr);
+						attriStrs.add(attrStr);
+					} while (matcher.find());
+				} else {
+					
+				}
+			}
+
+			if (attriStrs != null) {
+				Pattern attriDefPattern = Pattern
+						.compile(REGULAR_ATTRI_NAME_VALUE_EXISTENCE);
+				Pattern entityRefPattern = Pattern
+						.compile(REGULAR_ENTITY_REFERENCE);
+				Log.d(TAG, "match REGULAR_ATTRI_NAME_VALUE_EXISTENCE regular string-->\n" + REGULAR_ATTRI_NAME_VALUE_EXISTENCE);
+				for (String attrStr : attriStrs) {
+					Matcher attriMatcher = attriDefPattern.matcher(attrStr);
+					
+					if (attriMatcher.matches()) {
+						Log.d(TAG, "catch attri def-->" + attrStr);
+						for(int i = 0; i<attriMatcher.groupCount() + 1; i++){
+							Log.d(TAG, "catch attri group-->" + i + "; value-->" + attriMatcher.group(i));
+						}
+					} else if (entityRefPattern.matcher(attrStr).matches()) {
+						Log.d(TAG, "catch attri Ref-->" + attrStr);
+					}
+				}
+			}
+
 		}
-		Log.d(TAG, "matcher result-->" + matcher.matches());
 	}
 	
 }
